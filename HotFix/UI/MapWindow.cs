e46ee60dcff52e0d3bb288ab40ea4c8c7c58b43a
YangXvJie <2181264773@qq.com>
2026-01-06 20:36:11 +0800

    新项目

--- HotFix/UI/MapWindow.cs
+++ HotFix/UI/MapWindow.cs
@@ -11,49 +11,83 @@ namespace HotFix
     public class MapWindow : Window
     {
         private Button siYangChangBtn;
-        private Button fanTuChangBtn;
+        private Button fanYuChangBtn;
         private Button saiMaChangBtn;
         private Button closeBtn;
-        RawImage rawImage;
+
+        RawImage rapImage;
+
         Camera camera;
+
         public override void Awake(object param1 = null, object param2 = null, object param3 = null)
         {
-            GetALLComponent();
-            AddALLBtnistener();
+            GatAllComponent();
+            AddAllBtnListener();
         }
-        void AddALLBtnistener()
+
+        void AddAllBtnListener()
         {
-            AddButtonClickListener(closeBtn, CloseWndFunc);
+            AddButtonClickListener(closeBtn, () => { UIManager.instance.CloseWnd(this); });
             AddButtonClickListener(siYangChangBtn, GoToSiYangChang);
-            AddButtonClickListener(fanTuChangBtn, GoToSaiMachang);
-            AddButtonClickListener(saiMaChangBtn, GoToFanYunchang);
+            AddButtonClickListener(fanYuChangBtn, GoToSaiYangChang);
+            AddButtonClickListener(saiMaChangBtn, GoToFanYangChang);
+
 
         }
+
+        private void GoToFanYangChang()
+        {
+            Debug.Log("点击赛马场按钮");
+            RFrameWork.instance.OpenCommonConfirm("提示", "是否传送赛马场？", () =>
+            {
+                MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition, new Notification(2));
+
+            }, () => { });
+        }
+        //hotfix中
+        //弹窗逻辑
+        //网络 hotfix中所有webrequestmanager替换为//webrequestmananger  把所有web网络相关内容全部注释掉
+        //serrequestmanager中所有添加的事件全部注释掉
+
+        //asset里面的脚本 rframework
+        //rframework中 netmananger相关内容全部注释掉
+        //赛马场
+        private void GoToSaiYangChang()
+        {
+            Debug.Log("点击赛马场按钮");
+            RFrameWork.instance.OpenCommonConfirm("提示", "是否传送繁育场？", () =>
+            {
+                MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition, new Notification(3));
+
+            }, () => { });
+            // 
+
+        }
+
+        void GoToSiYangChang()
+        {
+            Debug.Log("点击赛马场按钮");
+            RFrameWork.instance.OpenCommonConfirm("提示", "是否传送饲养场？", () =>
+            {
+                MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition, new Notification(1));
+
+            }, () => { });
+        }
+
+
         void CloseWndFunc()
         {
             UIManager.instance.CloseWnd(this);
         }
-        void GoToSiYangChang()
-        {
-            MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition, new Notification(1));
-        }
-        void GoToSaiMachang()
-        {
-            MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition, new Notification(3));
-        }
-        void GoToFanYunchang()
-        {
-            MessageCenter.instance.Dispatch(MessageCenterEventID.PlayerChangePosition, new Notification(2));
-        }
-        void GetALLComponent()
+
+        private void GatAllComponent()
         {
             siYangChangBtn = m_Transform.Find("MapBack/siyangchang").GetComponent<Button>();
-            fanTuChangBtn = m_Transform.Find("MapBack/fanzhichang").GetComponent<Button>();
+            fanYuChangBtn = m_Transform.Find("MapBack/fanzhichang").GetComponent<Button>();
             saiMaChangBtn = m_Transform.Find("MapBack/saimachang").GetComponent<Button>();
             closeBtn = m_Transform.Find("MapBack/Close").GetComponent<Button>();
-            rawImage = m_Transform.Find("MapBack/RawImage").GetComponent<RawImage>();
+            rapImage = m_Transform.Find("MapBack/RawImage").GetComponent<RawImage>();
             camera = m_Transform.Find("Camera").GetComponent<Camera>();
-            Debug.Log("查找成功法！");
         }
     }
 }
