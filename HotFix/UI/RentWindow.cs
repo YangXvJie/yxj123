c70608f1253ac3f2d2178533bd7bda884abcba45
woleige12 <1950557441@qq.com>
2026-01-07 14:43:05 +0800

    租赁

---
+++ HotFix/UI/RentWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using W.GameFrameWork.ExcelTool;
+
+namespace HotFix
+{
+    class RentWindow : Window
+    {
+        public void RentPanel()
+        {
+            Window rentPanels = UIManager.instance.PopUpWnd(FilesName.RENTPANEL, true, false);
+        }
+        
+    }
+}
