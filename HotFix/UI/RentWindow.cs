a7590ae3fc5636eb3eb21243c85bda07d5ef2874
AIQIYA\aiqiy <aiqiya.qixing@qq.com>
2026-01-07 15:03:32 +0800

    关闭

--- HotFix/UI/RentWindow.cs
+++ HotFix/UI/RentWindow.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine.UI;
 using W.GameFrameWork.ExcelTool;
 
 namespace HotFix
 {
     class RentWindow : Window
     {
+        private Button returnBtn;
+        public override void Awake(object param1 = null, object param2 = null, object param3 = null)
+        {
+            GatAllComponent();
+            AddAllBtnListener();
+        }
+
+        private void AddAllBtnListener()
+        {
+            AddButtonClickListener(returnBtn, () => {UIManager.instance.CloseWnd(this); });
+        }
+
+        private void GatAllComponent()
+        {
+            returnBtn =m_Transform.Find("BackImg/CloseBtn").GetComponent<Button>();
+        }
+
         public void RentPanel()
         {
             Window rentPanels = UIManager.instance.PopUpWnd(FilesName.RENTPANEL, true, false);
